ae09b23cba701d11e6f69c120a065176ac7506da
8790 <2467042336@qq.com>
2022-02-25 04:21:24 +0800

    去掉黑框框，去掉命令行参数解析工具

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
+#![windows_subsystem = "windows"]
+
+use std::env;
 use clipboard_win::{formats, get_clipboard};
 use reqwest;
-use clap::{Parser};
-
-/// Push clipboard content to iOS
-#[derive(Parser, Debug)]
-struct Args {
-    /// target key
-    #[clap(short, long)]
-    key: String,
-}
-
 
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return;
+    }
+    
+    let key = &args[1];
+
     let context: String = match get_clipboard(formats::Unicode) {
         Ok(result) => result,
         Err(_) => String::from("剪贴板为空"),
     };
     // println!("{:?}", context);
 
-    let args = Args::parse();
     let url = format!(
         "https://home.promarcus.com:8100/{}/{}?copy={}&autoCopy=1&level=timeSensitive&isArchive=1",
-        args.key, &context, &context
+        key, &context, &context
     );
 
     reqwest::get(url).await.unwrap();
